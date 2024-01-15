8489f7801cbf87ec1182cee16db0643dd48f27fa
biozalp <biozalp@biozalp.com>
2024-01-15 04:04:23 +0300

    Initial Commit

---
+++ ThreadsReplica/ThreadsReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsReplicaApp.swift
+//  ThreadsReplica
+//
+//  Created by Berk Ilgar Özalp on 15.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
